1c50cb11efcb009bee4d7ba8136d28ae187257e5
--replace-all <gideonadurota@outlook.com>
2022-03-17 18:27:50 +0000

    make changes to modules to use for_each instead of count

--- main.tf
+++ main.tf
@@ -20,11 +20,15 @@ module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "2.66.0"
 
+  for_each = var.project
   cidr = var.vpc_cidr_block
 
   azs             = data.aws_availability_zones.available.names
-  private_subnets = slice(var.private_subnet_cidr_blocks, 0, var.private_subnets_per_vpc)
-  public_subnets  = slice(var.public_subnet_cidr_blocks, 0, var.public_subnets_per_vpc)
+  # private_subnets = slice(var.private_subnet_cidr_blocks, 0, var.private_subnets_per_vpc)
+  # public_subnets  = slice(var.public_subnet_cidr_blocks, 0, var.public_subnets_per_vpc)
+
+  private_subnets = slice(var.private_subnet_cidr_blocks, 0, each.value.private_subnets_per_vpc)
+  public_subnets  = slice(var.public_subnet_cidr_blocks, 0, each.value.public_subnets_per_vpc)
 
   enable_nat_gateway = true
   enable_vpn_gateway = false
@@ -34,22 +38,31 @@ module "app_security_group" {
   source  = "terraform-aws-modules/security-group/aws//modules/web"
   version = "3.17.0"
 
-  name        = "web-server-sg-${var.project_name}-${var.environment}"
-  description = "Security group for web-servers with HTTP ports open within VPC"
-  vpc_id      = module.vpc.vpc_id
+  for_each = var.Project
 
-  ingress_cidr_blocks = module.vpc.public_subnets_cidr_blocks
+  #name        = "web-server-sg-${var.project_name}-${var.environment}"
+  name        = "web-server-sg-${each.key}-${each.value.environment}"
+
+  description = "Security group for web-servers with HTTP ports open within VPC"
+  #vpc_id      = module.vpc.vpc_id
+  vpc_id      = module.vpc[each.key].vpc_id
+
+  #ingress_cidr_blocks = module.vpc.public_subnets_cidr_blocks
+  ingress_cidr_blocks = module.vpc[each.key].public_subnets_cidr_blocks
 }
 
 module "lb_security_group" {
   source  = "terraform-aws-modules/security-group/aws//modules/web"
   version = "3.17.0"
 
-  name = "load-balancer-sg-${var.project_name}-${var.environment}"
+  for_each = var.project
+
+  #name = "load-balancer-sg-${var.project_name}-${var.environment}"
+  name = "load-balancer-sg-${each.key}-${each.value.environment}"
 
   description = "Security group for load balancer with HTTP ports open within VPC"
-  vpc_id      = module.vpc.vpc_id
-
+  # vpc_id      = module.vpc.vpc_id
+  vpc_id      = module.vpc[each.key].vpc_id
   ingress_cidr_blocks = ["0.0.0.0/0"]
 }
 
@@ -64,11 +77,15 @@ module "elb_http" {
 
   # Comply with ELB name restrictions 
   # https://docs.aws.amazon.com/elasticloadbalancing/2012-06-01/APIReference/API_CreateLoadBalancer.html
-  name     = trimsuffix(substr(replace(join("-", ["lb", random_string.lb_id.result, var.project_name, var.environment]), "/[^a-zA-Z0-9-]/", ""), 0, 32), "-")
+  # name     = trimsuffix(substr(replace(join("-", ["lb", random_string.lb_id.result, var.project_name, var.environment]), "/[^a-zA-Z0-9-]/", ""), 0, 32), "-")
+  name     = trimsuffix(substr(replace(join("-", ["lb", random_string.lb_id.result, each.key, each.value.environment]), "/[^a-zA-Z0-9-]/", ""), 0, 32), "-")
+ 
   internal = false
 
-  security_groups = [module.lb_security_group.this_security_group_id]
-  subnets         = module.vpc.public_subnets
+  # security_groups = [module.lb_security_group.this_security_group_id]
+  # subnets         = module.vpc.public_subnets
+  security_groups = [module.lb_security_group[each.key].this_security_group_id]
+  subnets         = module.vpc[each.key].public_subnets
 
   number_of_instances = length(aws_instance.app)
   instances           = aws_instance.app.*.id
